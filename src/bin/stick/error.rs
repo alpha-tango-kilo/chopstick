060173d9c683ed6228def602328bd65a703700c6
alpha-tango-kilo <git@heyatk.com>
2021-11-24 15:12:14 +0000

    Write stick
    Add necessary error variants to StickError

--- src/bin/stick/error.rs
+++ src/bin/stick/error.rs
@@ -11,6 +11,10 @@ pub enum StickError {
     NotRecognised(PathBuf),
     Canonicalise(PathBuf, io::Error),
     IncompleteParts(Vec<OsString>),
+    CreateOriginal(PathBuf, io::Error),
+    ReadPart(PathBuf, io::Error),
+    WriteOriginal(io::Error),
+    DeletePart(PathBuf, io::Error),
 }
 
 impl StickError {
@@ -19,6 +23,10 @@ impl StickError {
             NotRecognised(_) => 1,
             Canonicalise(_, _) => 2,
             IncompleteParts(_) => 1,
+            CreateOriginal(_, _) => 2,
+            ReadPart(_, _) => 2,
+            WriteOriginal(_) => 2,
+            DeletePart(_, _) => 2,
         }
     }
 }
@@ -29,6 +37,10 @@ impl fmt::Display for StickError {
             NotRecognised(path) => write!(f, "Not recognised as a chopstick part: {}", path.to_string_lossy()),
             Canonicalise(path, why) => write!(f, "Failed to canonicalise {}: {}\nMaybe try giving the full path instead", path.to_string_lossy(), why),
             IncompleteParts(found) => write!(f, "Couldn't find all the parts to stick, only found the following: {:?}", found),
+            CreateOriginal(path, why) => write!(f, "Failed to create file {}: {}", path.to_string_lossy(), why),
+            ReadPart(path, why) => write!(f, "Couldn't read part {}: {}", path.to_string_lossy(), why),
+            WriteOriginal(why) => write!(f, "Couldn't write to original file: {}", why),
+            DeletePart(path, why) => write!(f, "Couldn't delete part {}: {}", path.to_string_lossy(), why),
         }
     }
 }
--- src/bin/stick/main.rs
+++ src/bin/stick/main.rs
@@ -1,6 +1,9 @@
 use crate::args::RunConfig;
+use crate::StickError::CreateOriginal;
 pub use error::*;
-use std::process;
+use std::fs::OpenOptions;
+use std::io::{Read, Write};
+use std::{fs, process};
 
 mod args;
 mod error;
@@ -14,6 +17,39 @@ fn main() {
 
 fn _main() -> Result<()> {
     let config = RunConfig::new()?;
-    println!("{:#?}", config);
+
+    let mut original_file = OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open(&config.original_file)
+        .map_err(|err| CreateOriginal(config.original_file.clone(), err))?;
+    let mut buffer = Vec::new();
+
+    config
+        .part_paths
+        .iter()
+        .try_for_each(|part_path| -> Result<()> {
+            use StickError::*;
+
+            // Step 1: read part into memory
+            let mut part = OpenOptions::new()
+                .read(true)
+                .open(part_path)
+                .map_err(|err| ReadPart(part_path.clone(), err))?;
+            part.read_to_end(&mut buffer)
+                .map_err(|err| ReadPart(part_path.clone(), err))?;
+
+            // Step 2: write buffer to original file
+            original_file.write_all(&buffer).map_err(WriteOriginal)?;
+
+            // Step 3: clear buffer
+            buffer.clear();
+
+            // Step 4: delete part file
+            fs::remove_file(part_path)
+                .map_err(|err| DeletePart(part_path.clone(), err))?;
+
+            Ok(())
+        })?;
     Ok(())
 }

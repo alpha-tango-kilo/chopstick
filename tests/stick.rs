0fde03ee4a770432a09bcb83f81294fefe97485d
alpha-tango-kilo <git@heyatk.com>
2021-11-30 17:35:44 +0000

    Write test more properly
    Use slices of TEST_BYTES instead of having a separate variable

--- tests/stick.rs
+++ tests/stick.rs
@@ -5,18 +5,6 @@ use assert_fs::TempDir;
 use chopstick::EXTENSION_PREFIX;
 
 const FILE_NAME: &str = "stick_me";
-const PART_BYTES: [[u8; 10]; 10] = [
-    [164, 108, 152, 89,  172, 190, 243, 194, 202, 143],
-    [158, 187, 192, 211, 33,  195, 34,  27,  108, 57 ],
-    [177, 144, 199, 135, 136, 143, 57,  246, 45,  100],
-    [247, 59,  163, 101, 168, 68,  244, 190, 137, 114],
-    [216, 67,  112, 196, 124, 170, 74,  78,  35,  53 ],
-    [204, 163, 235, 101, 179, 30,  51,  41,  9,   199],
-    [125, 89,  132, 75,  221, 221, 102, 190, 51,  255],
-    [246, 185, 199, 168, 19,  14,  9,   205, 59,  31 ],
-    [124, 106, 58,  100, 67,  81,  95,  200, 96,  1  ],
-    [205, 206, 67,  38,  21,  224, 247, 75,  1,   131],
-];
 const TEST_BYTES: [u8; 100] = [
     164, 108, 152, 89, 172, 190, 243, 194, 202, 143, 158, 187, 192, 211, 33,
     195, 34, 27, 108, 57, 177, 144, 199, 135, 136, 143, 57, 246, 45, 100, 247,
@@ -30,14 +18,15 @@ const TEST_BYTES: [u8; 100] = [
 #[test]
 fn combine() {
     let temp_dir = TempDir::new().unwrap();
-    PART_BYTES
-        .iter()
-        .enumerate()
-        .try_for_each(|(part_no, tenple)| {
+    (0..10)
+        .into_iter()
+        .map(|n| (n + 1, &TEST_BYTES[n * 10..n * 10 + 10]))
+        .try_for_each(|(part_no, slice)| {
             let child_path =
-                format!("{}.{}{:0>2}", FILE_NAME, EXTENSION_PREFIX, part_no + 1);
+                format!("{}.{}{:0>2}", FILE_NAME, EXTENSION_PREFIX, part_no);
+            println!("Part {}, bytes {:?}", part_no, slice);
             let part = temp_dir.child(&child_path);
-            part.write_binary(tenple)
+            part.write_binary(slice)
         })
         .expect("Failed to setup test: writing temp file");
 

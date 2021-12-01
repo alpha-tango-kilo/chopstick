ae1868ae8ec7fcb0d399159e1deeab33106527c5
alpha-tango-kilo <git@heyatk.com>
2021-12-01 17:45:08 +0000

    Randomise number of parts

--- tests/stress.rs
+++ tests/stress.rs
@@ -3,47 +3,66 @@ use assert_cmd::Command;
 use assert_fs::prelude::*;
 use assert_fs::TempDir;
 use chopstick::EXTENSION_PREFIX;
-use rand::{thread_rng, RngCore};
+use rand::{thread_rng, Rng, RngCore};
+use std::cmp::min;
 use std::fs;
 
 const FILE_NAME: &str = "chopnplop";
 const FIVE_HUNGE_KIB: usize = 500 * 1024;
 
 #[test]
-fn combined() {
+fn num_parts() {
+    let mut rng = thread_rng();
     // Setup
     let temp_dir = TempDir::new().unwrap();
     let temp_file = temp_dir.child(FILE_NAME);
     let mut file_bytes = [0u8; FIVE_HUNGE_KIB];
-    thread_rng().fill_bytes(&mut file_bytes);
+    rng.fill_bytes(&mut file_bytes);
     temp_file
         .write_binary(&file_bytes)
         .expect("Failed to write test bytes to temp file");
 
+    let num_parts: usize = rng.gen_range(5..=1000);
+    println!("Using {} parts", num_parts);
+    // Round up division wooo
+    let part_size =
+        FIVE_HUNGE_KIB / num_parts + (FIVE_HUNGE_KIB % num_parts != 0) as usize;
+    let zero_pad = num_parts / 10 + 1;
+
     // Chop
-    // TODO: random splitting opcode & operand
     Command::cargo_bin("chop")
         .unwrap()
-        .args(&["-n", "10", &temp_file.path().to_string_lossy()])
+        .args(&[
+            "-n",
+            &num_parts.to_string(),
+            &temp_file.path().to_string_lossy(),
+        ])
         .unwrap()
         .assert()
         .success();
 
     // Check intermediary parts
-    (0..10)
+    (0..num_parts)
         .into_iter()
-        .map(|n| (n + 1, n * FIVE_HUNGE_KIB / 10))
+        .map(|n| (n + 1, n * part_size))
         .for_each(|(part_no, file_bytes_offset)| {
-            let child_path =
-                format!("{}.{}{:0>2}", FILE_NAME, EXTENSION_PREFIX, part_no);
+            let child_path = format!(
+                "{}.{}{:0>width$}",
+                FILE_NAME,
+                EXTENSION_PREFIX,
+                part_no,
+                width = zero_pad
+            );
             let part = temp_dir.child(&child_path);
             let part_bytes = fs::read(part.path())
                 .expect(&format!("Unable to find/read {:?}", &part.path()));
+            let end_index =
+                min(file_bytes.len(), file_bytes_offset + part_size);
             assert_eq!(
                 part_bytes.as_slice(),
-                &file_bytes[file_bytes_offset
-                    ..file_bytes_offset + FIVE_HUNGE_KIB / 10],
-                "File contents differs",
+                &file_bytes[file_bytes_offset..end_index],
+                "File contents differs in part {}",
+                part_no,
             );
         });
 

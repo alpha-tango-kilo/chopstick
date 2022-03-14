0f42a83d025eed73482523be73bd4f5cbfff8d0f
alpha-tango-kilo <git@heyatk.com>
2022-03-14 17:17:10 +0000

    Implemented buffered, ranged reading for chop
    Create RangeReader to read ranges of bytes in files

--- src/bin/chop/main.rs
+++ src/bin/chop/main.rs
@@ -1,11 +1,14 @@
 use crate::ChopError::*;
 use args::RunConfig;
-use chopstick::{digits, sufficient_disk_space};
+use chopstick::{digits, max_buffer_size, sufficient_disk_space};
 pub use error::*;
 pub use lib::*;
-use std::fs::OpenOptions;
+use std::cmp::min;
+use std::fs::{File, OpenOptions};
 use std::io::{Read, Seek, SeekFrom, Write};
-use std::{fs, mem, process};
+use std::ops::Range;
+use std::path::Path;
+use std::{fs, io, mem, process};
 
 mod args;
 mod error;
@@ -20,12 +23,13 @@ fn main() {
 
 fn _main() -> Result<()> {
     let config = RunConfig::new()?;
+    let file_size = fs::metadata(&config.path)?.len();
 
     // Check if there is sufficient disk space available
     let space_needed = if !config.retain {
         config.split.part_size
     } else {
-        fs::metadata(&config.path)?.len()
+        file_size
     };
     match sufficient_disk_space(&config.path, space_needed) {
         Ok(true) => {
@@ -38,11 +42,8 @@ fn _main() -> Result<()> {
     }
 
     // Cast is saturating if part_size > usize::MAX
-    let mut buffer = Vec::with_capacity(config.split.part_size as usize);
-    let mut handle = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .open(&config.path)?;
+    let buffer_size = min(config.split.part_size, max_buffer_size()) as usize;
+    let mut reader = RangeReader::new(&config.path, buffer_size)?;
     let zero_pad_width = digits(config.split.num_parts) as usize;
 
     if config.verbose {
@@ -51,83 +52,48 @@ fn _main() -> Result<()> {
 
     (0..config.split.num_parts)
         .into_iter()
+        // Have to make parts backwards because we can only truncate the
+        // original file
         .rev()
         .map(|part| {
-            (
-                part * config.split.part_size,
-                get_part_path_buf(&config.path, part + 1, zero_pad_width),
-            )
+            let start = part * config.split.part_size;
+            let end = min(start + config.split.part_size, file_size);
+            let part_path =
+                get_part_path_buf(&config.path, part + 1, zero_pad_width);
+            (start, end, part_path)
         })
-        .try_for_each(|(byte_offset, part_path)| -> Result<()> {
-            // Step 1: Get the source file handle pointed at the right place
-            handle
-                .seek(SeekFrom::Start(byte_offset))
-                .expect("Arithmetic error, seek outside of file");
-
-            // Step 2: Create part file
-            let part_file = if !config.dry_run {
-                OpenOptions::new()
-                    .write(true)
-                    .create_new(true)
-                    .open(&part_path)
-                    .map_err(|err| {
-                        use std::io::ErrorKind::*;
-                        match err.kind() {
-                            AlreadyExists => {
-                                PartFileAlreadyExists(part_path.clone())
-                            }
-                            _ => err.into(),
+        .try_for_each(|(start, end, part_path)| -> Result<()> {
+            // TODO: verbose & dry-run
+            let mut part_file = OpenOptions::new()
+                .write(true)
+                .create_new(true)
+                .open(&part_path)
+                .map_err(|err| {
+                    use std::io::ErrorKind::*;
+                    match err.kind() {
+                        AlreadyExists => {
+                            PartFileAlreadyExists(part_path.clone())
                         }
-                    })?
-                    .into()
-            } else {
-                None
-            };
+                        _ => err.into(),
+                    }
+                })?;
 
-            if config.verbose {
-                eprintln!(
-                    "Created part file {}",
-                    part_path.file_name().unwrap().to_string_lossy()
-                );
-            }
-
-            // Step 3: read to end of source file into the buffer
-            if !config.dry_run {
-                handle.read_to_end(&mut buffer).map_err(FailedToReadPart)?;
-            }
-            if config.verbose {
-                let amount = bytesize::to_string(buffer.len() as u64, true);
-                eprintln!("Read {amount} bytes into buffer");
-            }
-
-            // Step 4: write buffer to part file, then clear buffer
-            if !config.dry_run {
+            reader.change_range(start..end)?;
+            while let Some(bytes) = reader.read().map_err(FailedToReadPart)? {
                 part_file
-                    .unwrap()
-                    .write_all(&buffer)
+                    .write_all(bytes)
                     .map_err(|err| FailedToWritePart(part_path.clone(), err))?;
-                buffer.clear();
-            }
-            if config.verbose {
-                eprintln!("Wrote buffer to part file");
             }
 
-            // Step 5: truncate source file
             if !config.retain {
-                if !config.dry_run {
-                    handle.set_len(byte_offset).map_err(FailedToTruncate)?;
-                }
-                if config.verbose {
-                    eprintln!("Truncated original file");
-                }
+                reader.file.set_len(end).map_err(FailedToTruncate)?;
             }
-
             Ok(())
         })?;
 
-    // Drop isn't strictly necessary but saves me trying to use it on a
-    // soon-to-be deleted file
-    mem::drop(handle);
+    // Drop isn't strictly necessary but saves me trying to use it after the
+    // file is deleted
+    mem::drop(reader);
     if !config.retain {
         if !config.dry_run {
             fs::remove_file(&config.path).map_err(FailedToDeleteOriginal)?;
@@ -142,4 +108,41 @@ fn _main() -> Result<()> {
     }
 
     Ok(())
+}
+
+struct RangeReader {
+    pub file: File,
+    buffer: Vec<u8>,
+    end: u64,
+}
+
+impl RangeReader {
+    fn new<P: AsRef<Path>>(path: P, buffer_size: usize) -> io::Result<Self> {
+        let file = OpenOptions::new().read(true).write(true).open(path)?;
+        Ok(RangeReader {
+            file,
+            buffer: vec![0; buffer_size],
+            end: 0,
+        })
+    }
+
+    fn change_range(&mut self, range: Range<u64>) -> io::Result<()> {
+        self.end = range.end;
+        self.file.seek(SeekFrom::Start(range.start))?;
+        Ok(())
+    }
+
+    fn read(&mut self) -> io::Result<Option<&[u8]>> {
+        if self.bytes_left()? == 0 {
+            Ok(None)
+        } else {
+            let bytes_read = self.file.read(&mut self.buffer)?;
+            Ok(Some(&self.buffer[..bytes_read]))
+        }
+    }
+
+    // stream_position requires mutability
+    fn bytes_left(&mut self) -> io::Result<u64> {
+        self.file.stream_position().map(|index| self.end - index)
+    }
 }
